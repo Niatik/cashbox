b3a1a1d2cd5238161ad6e6695a641b1f8fd97cbc
Nikita <11559709+Niatik@users.noreply.github.com>
2024-08-15 19:06:43 +0500

    Expand ServiceTest
      - Import necessary classes and functions
      - Add test case to verify listing of services
      - Implement Livewire test for ServiceResource ListServices page.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,13 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
         Service::factory(10)->create();
 
         User::factory([
             'name' => 'Nikita',
             'email' => 'nikita.dragunov@gmail.com',
-            'password' => Hash::make('b@stet'),
+            'password' => '$2y$12$YmvdDfOvnYf9MSfC./DTkeeVLtNCgV0tWQx5cF.OQab1Wjwi4bIjO',
         ])->create();
     }
 }
--- tests/Feature/ServiceTest.php
+++ tests/Feature/ServiceTest.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Filament\Resources\ServiceResource;
+use App\Models\Service;
 use App\Models\User;
 use Filament\Facades\Filament;
+
+use Livewire\Livewire;
+use function Pest\Livewire\livewire;
+
 
 beforeEach(function () {
     $this->actingAs(
@@ -12,6 +17,13 @@ beforeEach(function () {
 
 it('can render page', function () {
     $this->get(ServiceResource::getUrl('index'))->assertSuccessful();
+});
+
+it('can list services', function () {
+    $services = Service::factory()->count(10)->create();
+
+    livewire(ServiceResource\Pages\ListServices::class)
+        ->assertCanSeeTableRecords($services);
 });
 
 
