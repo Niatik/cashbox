70edab3283dbbb1f3c4d24c97d30e72bbd0972de
Nikita <11559709+Niatik@users.noreply.github.com>
2024-08-16 16:05:52 +0500

    Refactor DatabaseSeeder: Remove unused Hash facade import
    - Delete the 'use Illuminate\Support\Facades\Hash;' statement

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,8 +6,6 @@ use App\Models\Service;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
-
 class DatabaseSeeder extends Seeder
 {
     /**
