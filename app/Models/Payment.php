a8a886e05918f4db4bde0b393e13d50f6bfe8262
Nikita <11559709+Niatik@users.noreply.github.com>
2024-09-01 16:43:22 +0500

    Add Payment model, factory, and migration
    This commit introduces the Payment functionality to the application:
    
    1. Created Payment model (app/Models/Payment.php):
       - Defined fillable attributes
       - Established relationships with Order and PaymentType models;
    
    2. Added PaymentFactory (database/factories/PaymentFactory.php);
    
    3. Created migration for payments table (database/migrations/2024_08_16_124756_create_payments_table.php).

---
+++ app/Models/Payment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'payment_type_id',
+        'payment_date',
+        'payment_amount',
+    ];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function payment_type(): BelongsTo
+    {
+        return $this->belongsTo(PaymentType::class);
+    }
+}
