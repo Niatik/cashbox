80a070cc9e22cac5eff70bcf9595cec8ff10b65b
Nikita <11559709+Niatik@users.noreply.github.com>
2025-02-20 16:37:38 +0500

    feat(ViewCashReport): Add title for cash report view page
    Added a descriptive Russian title 'Отчет за день' to the cash report view page while removing unused Filament\Actions import.

--- app/Filament/Resources/CashReportResource/Pages/ViewCashReport.php
+++ app/Filament/Resources/CashReportResource/Pages/ViewCashReport.php
@@ -3,10 +3,11 @@
 namespace App\Filament\Resources\CashReportResource\Pages;
 
 use App\Filament\Resources\CashReportResource;
-use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewCashReport extends ViewRecord
 {
+    protected static ?string $title = 'Отчет за день';
+
     protected static string $resource = CashReportResource::class;
 }
