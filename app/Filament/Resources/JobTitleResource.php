d4e9bb84f53ac4dcbe625c40cf9e7dd7cb69fe3b
Nikita <11559709+Niatik@users.noreply.github.com>
2026-01-30 19:10:03 +0500

    feat(resources): add `JobTitleResource` with pages and relation manager
    - Created `JobTitleResource` for managing job titles, including listing, creation, and editing pages.
    - Implemented `RatesRelationManager` for handling related rates.
    - Configured navigation grouping and custom labels for localization support.

---
+++ app/Filament/Resources/JobTitleResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\JobTitleResource\Pages;
+use App\Filament\Resources\JobTitleResource\RelationManagers\RatesRelationManager;
+use App\Models\JobTitle;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class JobTitleResource extends Resource
+{
+    protected static ?string $model = JobTitle::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $label = '';
+
+    protected static ?string $pluralLabel = 'Должности';
+
+    protected static ?string $navigationGroup = 'Справочники';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->label('Название')
+                    ->maxLength(255)
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->label('Название')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make()->label('Изменить')->hiddenLabel(true),
+                Tables\Actions\DeleteAction::make()->label('Удалить')->hiddenLabel(true),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make()->label('Удалить'),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            RatesRelationManager::class,
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListJobTitles::route('/'),
+            'create' => Pages\CreateJobTitle::route('/create'),
+            'edit' => Pages\EditJobTitle::route('/{record}/edit'),
+        ];
+    }
+}
