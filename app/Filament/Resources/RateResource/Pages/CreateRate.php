b6610c1aa936ad7926a9d3982bba6b4ecf5d5554
Nikita <11559709+Niatik@users.noreply.github.com>
2026-01-30 19:40:27 +0500

    feat(resources): add `RateResource` with pages and relation manager
    - Introduced `RateResource` for managing rates, including list, create, and edit pages.
    - Implemented `RateRatiosRelationManager` for handling related rate ratios.
    - Configured navigation, labels, and actions for improved usability and localization support.

---
+++ app/Filament/Resources/RateResource/Pages/CreateRate.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\RateResource\Pages;
+
+use App\Filament\Resources\RateResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRate extends CreateRecord
+{
+    protected static string $resource = RateResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
---
+++ app/Filament/Resources/RateResource/Pages/EditRate.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\RateResource\Pages;
+
+use App\Filament\Resources\RateResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditRate extends EditRecord
+{
+    protected static string $resource = RateResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make()->label('Удалить ставку'),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
