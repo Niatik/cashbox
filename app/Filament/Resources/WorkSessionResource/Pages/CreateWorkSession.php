d768681799e01ff9e257d34d478055f629cdc736
Nikita <11559709+Niatik@users.noreply.github.com>
2026-02-24 15:39:01 +0500

    feat(work-session): rename Create button to Старт and remove Create Another
    Override getFormActions() in CreateWorkSession page to return only a single submit action labeled 'Старт', removing the 'Create another' button.

--- app/Filament/Resources/WorkSessionResource/Pages/CreateWorkSession.php
+++ app/Filament/Resources/WorkSessionResource/Pages/CreateWorkSession.php
@@ -8,4 +8,12 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateWorkSession extends CreateRecord
 {
     protected static string $resource = WorkSessionResource::class;
+
+    protected function getFormActions(): array
+    {
+        return [
+            $this->getCreateFormAction()->label('Старт'),
+            $this->getCancelFormAction()->label('Отмена'),
+        ];
+    }
 }
