aba7f93d68b7c3dcaf8cfba42d0f17aa6e04aa71
Nikita <11559709+Niatik@users.noreply.github.com>
2026-02-24 16:32:58 +0500

    feat(models): add `ExpenseWorkSession` model
    - Created `ExpenseWorkSession` model with relationships to `WorkSession` and `ExpenseType`.
    - Added `fillable` and `casts` properties, including `MoneyCast` for `expense_amount`.

---
+++ app/Models/ExpenseWorkSession.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use App\Casts\MoneyCast;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ExpenseWorkSession extends Model
+{
+    /** @use HasFactory<\Database\Factories\ExpenseWorkSessionFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'work_session_id',
+        'expense_type_id',
+        'amount',
+    ];
+
+    protected $casts = [
+        'expense_amount' => MoneyCast::class,
+    ];
+
+
+    public function workSession(): BelongsTo
+    {
+        return $this->belongsTo(WorkSession::class);
+    }
+
+    public function expenseType(): BelongsTo
+    {
+        return $this->belongsTo(ExpenseType::class);
+    }
+}
